e0622339af0189ff5d2333d2e4ad6cd4d11a9d6b
Alejo Max Barugel <alejobaruja@gmail.com>
2026-09-02 10:46:47 -0300

    nuevo proeycto subido

--- assets/js/data.js
+++ assets/js/data.js
@@ -30,7 +30,7 @@ const PROYECTOS = [
     titulo: "Mis Tableros",
     descripcion: "Herramienta para crear tableros personalizados y realizar sorteos de forma fácil y rápida.",
     imagen: "assets/img/mis_tableros.png",
-    link: "https://tablero-de-sorteo.vercel.app",
+    link: "misTableros.html",
     badgeTipo: "FullStack",
     tecnologias: ["Java Script", "HTML", "CSS"]
   }
